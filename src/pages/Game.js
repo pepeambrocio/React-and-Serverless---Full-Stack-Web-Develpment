cb704704b0f6f68e3512713db5a7a93fdd69caa7
JOSE CRUZ AMBROCIO <ambrociojc@gmail.com>
2020-06-01 21:56:32 -0500

    Styled the game page

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,9 +1,22 @@
 import React from "react";
+import {
+  StyledGame,
+  StyledScore,
+  StyleTimer,
+  StyledCharacter,
+} from "../styled/Game";
+import { Strong } from "../styled/Random";
 
 export default function Game() {
   return (
-    <div>
-      <h1>Game</h1>
-    </div>
+    <StyledGame>
+      <StyledScore>
+        Score:<Strong>0</Strong>
+      </StyledScore>
+      <StyledCharacter>A</StyledCharacter>
+      <StyleTimer>
+        Time: <Strong>00: 000</Strong>
+      </StyleTimer>
+    </StyledGame>
   );
 }
