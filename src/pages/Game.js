04278e09e782efb23b54cbd9ecabf749d7dcd431
JOSE CRUZ AMBROCIO <ambrociojc@gmail.com>
2020-06-07 23:21:08 -0500

    implemented the timer

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -7,17 +7,45 @@ import {
 } from "../styled/Game";
 import { Strong } from "../styled/Random";
 
-export default function Game() {
+export default function Game({ history }) {
   const [score, setScore] = useState(1);
+  const MAX_SECONDS = 5;
+  const [ms, setMs] = useState(0);
+  const [seconds, setSeconds] = useState(MAX_SECONDS);
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      setScore((prevScore) => prevScore + 1);
-    }, 100);
-    return () => {
-      clearInterval(interval);
-    };
-  }, [score]);
+    const currentTime = new Date();
+    const interval = setInterval(() => updateTime(currentTime), 1);
+    return () => clearInterval(interval);
+  }, []);
+
+  const updateTime = (startTime) => {
+    const endDate = new Date();
+    const msPassedStr = (endDate.getTime() - startTime.getTime()).toString();
+    const formattedMSString = ("0000" + msPassedStr).slice(-5);
+    const updatedSeconds =
+      MAX_SECONDS - parseInt(formattedMSString.substring(0, 2)) - 1;
+    const updatedMs =
+      1000 -
+      parseInt(formattedMSString.substring(formattedMSString.length - 3));
+
+    setSeconds(addLeadingZeros(updatedSeconds, 2));
+    setMs(addLeadingZeros(updatedMs, 3));
+  };
+
+  const addLeadingZeros = (num, length) => {
+    let zeros = "";
+    for (let i = 0; i < length; i++) {
+      zeros += "0";
+    }
+    return (zeros + num).slice(-length);
+  };
+
+  useEffect(() => {
+    if (seconds <= -1) {
+      history.push("/gameOver");
+    }
+  }, [seconds, ms]);
 
   return (
     <StyledGame>
@@ -26,7 +54,10 @@ export default function Game() {
       </StyledScore>
       <StyledCharacter>A</StyledCharacter>
       <StyleTimer>
-        Time: <Strong>00: 000</Strong>
+        Time:{" "}
+        <Strong>
+          {seconds}: {ms}
+        </Strong>
       </StyleTimer>
     </StyledGame>
   );
