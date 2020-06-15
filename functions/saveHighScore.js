1e219da8a7cf0033124e037d396b654591f7b52b
JOSE CRUZ AMBROCIO <ambrociojc@gmail.com>
2020-06-15 16:29:50 -0500

    refactoring out serveless functions with an airtable utils file

--- functions/saveHighScore.js
+++ functions/saveHighScore.js
@@ -1,12 +1,4 @@
-require("dotenv").config();
-const Airtable = require("airtable");
-
-Airtable.configure({
-  apiKey: process.env.AIRTABLE_API_KEY,
-});
-
-const base = require("airtable").base(process.env.AIRTABLE_BASE);
-const table = base.table(process.env.AIRTABLE_TABLE);
+const { table, getHighScores } = require("./utils/airtable");
 
 exports.handler = async (event) => {
   if (event.httpMehod !== "POST") {
@@ -25,18 +17,10 @@ exports.handler = async (event) => {
   }
 
   try {
-    const records = await table
-      .select({
-        sort: [{ field: "name", direction: "desc" }],
-      })
-      .firstPage();
-    const formattedRecords = records.map((record) => ({
-      id: record.id,
-      fields: record.fields,
-    }));
+    const records = await getHighScores(false);
     //we've got formatted recodrs in order of scroes highest to lowest
     //the lowest record is at index 9
-    const lowestRecord = formattedRecords[9];
+    const lowestRecord = records[9];
     if (
       typeof lowestRecord.fields.score === "undefined" ||
       score > lowestRecord.fields.score
