674467b979ce41468ad1f0f25abe9d96afae2641
JOSE CRUZ AMBROCIO <ambrociojc@gmail.com>
2020-06-29 23:33:09 -0500

    switching of themes if working

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,8 +8,9 @@ import {
 } from "../styled/Navbar";
 import { Accent } from "../styled/Random";
 import { useAuth0 } from "../auth";
+import useTheme from "../hooks/UseTheme";
 
-export default function Navbar() {
+export default function Navbar({ toggleTheme }) {
   const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
   return (
     <StyledNavbar>
@@ -35,6 +36,7 @@ export default function Navbar() {
             <button onClick={logout}>Logout</button>
           </li>
         )}
+        <button onClick={toggleTheme}>Toggle Theme</button>
       </StyledNavItems>
     </StyledNavbar>
   );
